019c76699eab999fa5ba4816a9a8323d3a7ef268
姚鑫 <yaoxin@nmy.cn>
2024-09-20 16:40:48 +0800

    添加了log记录日志的常见情况的写法

--- LogWithStringTemplate/Program.cs
+++ LogWithStringTemplate/Program.cs
@@ -1,14 +1,15 @@
 ﻿using LogWithStringTemplate.CallerMemberAttribute;
 using Serilog;
+using Serilog.Core;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .WriteTo.Console()
     .CreateLogger();
 
-
-CallerOne.GetCallerInfo();
-CallerTwo.GetCallerInfo();
+//记录堆栈信息
+//CallerOne.GetCallerInfo();
+//CallerTwo.GetCallerInfo();
 
 
 Console.WriteLine("Ready To Log");
@@ -36,6 +37,22 @@ for (int i = 0; i < 12345; i++)
         Serilog's support for structured logging shines when instrumenting complex, distributed, and asynchronous applications and systems.
         """, i);
 }
+
+#region 不使用字符串插值，记录日志的一些情况的写法
+var strList = new List<string> { "a", "b", "c" };
+var strList2 = new List<string> { "x", "y", "z" };
+int num = 3;
+
+var p = new
+{
+    Name = "Jim",
+    Age = 13
+};
+Log.Information("The list is {StrList}", string.Join(",", strList));
+Log.Information("The list is {StrList},num = {Num}", string.Join(",", strList), num);
+Log.Information("The list is {StrList2},list2 is {StrList}", string.Join(",", strList), string.Join(",", strList2));
+Log.Information("p = {p}", p); 
+#endregion
 
 Console.WriteLine();
 Console.WriteLine("Ready To Log");
