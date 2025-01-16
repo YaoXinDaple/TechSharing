1dc9c4cb6aa4897130d57cc9710138872efbd0d1
yaoxindaple <dapleyx@gmail.com>
2025-01-16 11:32:15 +0800

    正则表达式内存占用测试

--- TaxItemCategorySync/Program.cs
+++ TaxItemCategorySync/Program.cs
@@ -1,10 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 using TaxItemCategorySync;
 
 Console.WriteLine("Hello, World!");
 
-
-
-BenchmarkDotNet.Running.BenchmarkRunner.Run<IsValidEmailBenchmark>();
+BenchmarkDotNet.Running.BenchmarkRunner.Run<RegexMatchBenchmark>();
 
 Console.ReadLine();
+
+
+ bool IsValidEmailGroup([NotNullWhen(true)] ReadOnlySpan<char> str)
+{
+    if (str.IsWhiteSpace())
+    {
+        return false;
+    }
+
+    Span<Range> ranges = stackalloc Range[3];
+    ReadOnlySpan<char> separators = [';','；'];
+
+    int count = str.SplitAny(ranges, separators);
+    if (count > 3)
+    {
+        throw new ArgumentException($"邮箱地址应该不超过3个");
+    }
+    for (int i = 0; i < count; i++)
+    {
+        if (!IsEmail(str[ranges[i]]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+bool IsEmail([NotNullWhen(true)]  ReadOnlySpan<char> str)
+{
+    if (str.IsWhiteSpace())
+    {
+        return false;
+    }
+    if (!Regex.IsMatch(str, @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$"))
+    {
+        return false;
+    }
+    return true;
+}
+
+List<string> GetValidEmails([NotNullWhen(true)] string? str)
+{
+    if (string.IsNullOrWhiteSpace(str))
+    {
+        return [];
+    }
+
+    var strSpan = str.AsSpan();
+    Span<Range> ranges = stackalloc Range[3];
+    ReadOnlySpan<char> separators = [';', '；'];
+
+    //ranges从常量上定义了最大的邮箱地址数量之后，这里只会读取常量中设置的数量
+    int count = strSpan.SplitAny(ranges, separators);
+    var emailList = new List<string>(count);
+
+    for (int i = 0; i < count; i++)
+    {
+        if (!IsEmail(strSpan[ranges[i]]))
+        {
+            emailList.Add(strSpan[ranges[i]].ToString());
+        }
+    }
+    return emailList;
+}
---
+++ TaxItemCategorySync/RegexMatchBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text.RegularExpressions;
+
+namespace TaxItemCategorySync
+{
+    [MemoryDiagnoser]
+    public class RegexMatchBenchmark
+    {
+        //结论：对单个字符串进行正则匹配，没有额外的内存分配
+
+        public string PhoneNumber { get; set; } = "13800138000";
+
+        [Benchmark]
+        public bool IsPhoneNumber()
+        {
+            if (string.IsNullOrWhiteSpace(PhoneNumber))
+            {
+                return false;
+            }
+
+            // 中国手机号码
+            if (!Regex.IsMatch(PhoneNumber, @"^1[3-9]\d{9}$"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
