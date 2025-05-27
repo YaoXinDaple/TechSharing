52ab5949b8402cd2e18d2c0cf90d583a1f72a3c3
YaoXinDaple <dapleyx@gmail.com>
2025-05-28 00:25:12 +0800

    zlinq

---
+++ ZlinqCompareWithLinq/LinqAllocateBenchmark.cs
@@ -0,0 +1,94 @@
+﻿using BenchmarkDotNet.Attributes;
+using ZLinq;
+
+namespace ZlinqCompareWithLinq
+{
+    [MemoryDiagnoser]
+    public class LinqAllocateBenchmark
+    {
+        private List<GLProjectType> projectTypes = new List<GLProjectType>();
+        private List<GLProject> projects = new List<GLProject>();
+        private Dictionary<(string, string), GLProject> projectTypeCodeAndNameValueTupleKeyDict = new Dictionary<(string, string), GLProject>();
+        private Dictionary<string, GLProject> projectTypeCodeAndNameStringKeyDict = new Dictionary<string, GLProject>();
+        [GlobalSetup]
+        public void Setup()
+        {
+            //创建10个GLProjectType 实例
+            for (int i = 0; i < 10; i++)
+            {
+                projectTypes.Add(new GLProjectType
+                {
+                    ProjectTypeCode = $"Code{i}",
+                    ProjectTypeName = $"Name{i}"
+                });
+            }
+            //为 projectTypes 中每个实例生成 10-50个 GLProject 实例
+            for (int i = 0; i < projectTypes.Count; i++)
+            {
+                var projectType = projectTypes[i];
+                var projectCount = new Random().Next(10, 500);
+                for (int j = 0; j < projectCount; j++)
+                {
+                    projects.Add(new GLProject
+                    {
+                        ProjectTypeCode = projectType.ProjectTypeCode,
+                        ProjectName = $"{projectType.ProjectTypeCode}-ProjectName{j}",
+                        ProjectCode = $"{projectType.ProjectTypeCode}-ProjectCode{j}"
+                    });
+                }
+            }
+        }
+
+        [Benchmark]
+        public void WhereThenCount()
+        {
+            var count = projects.Where(p => p.ProjectTypeCode == "Code0").Count();
+        }
+
+        [Benchmark]
+        public void StrictCount()
+        {
+            var count = projects.Count(p => p.ProjectTypeCode == "Code0");
+        }
+
+        [Benchmark]
+        public void AsValueEnumerableWhereThenCount()
+        {
+            var count = projects.AsValueEnumerable().Where(p => p.ProjectTypeCode == "Code0").Count();
+        }
+
+
+
+        //[Benchmark]
+        //public void LinqUseString()
+        //{
+        //    projects.Select(p => projectTypeCodeAndNameStringKeyDict.TryAdd(p.ProjectTypeCode + p.ProjectName, p)).ToList();
+        //}
+        //[Benchmark]
+        //public void LinqUseValueTuple()
+        //{
+        //    projects.Select(p => projectTypeCodeAndNameValueTupleKeyDict.TryAdd((p.ProjectTypeCode, p.ProjectName), p)).ToList();
+        //}
+
+        //[Benchmark]
+        //public void UseManualLoopString()
+        //{
+        //    for (int i = 0; i < projects.Count; i++)
+        //    {
+        //        var p = projects[i];
+        //        projectTypeCodeAndNameStringKeyDict.TryAdd(p.ProjectTypeCode + p.ProjectName, p);
+        //    }
+        //}
+
+        //[Benchmark]
+        //public void UseManualLoopValueTuple()
+        //{
+        //    for (int i = 0; i < projects.Count; i++)
+        //    {
+        //        var p = projects[i];
+        //        projectTypeCodeAndNameValueTupleKeyDict.TryAdd((p.ProjectTypeCode, p.ProjectName), p);
+        //    }
+        //}
+
+    }
+}
---
+++ ZlinqCompareWithLinq/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ZlinqCompareWithLinq;
+
+Console.WriteLine("Hello, World!");
+
+
+
+BenchmarkDotNet.Running.BenchmarkRunner.Run<LinqAllocateBenchmark>();
+
+Console.ReadLine();
+
+//创建10个GLProjectType 实例
+var projectTypes = new List<GLProjectType>();
+for (int i = 0; i < 10; i++)
+{
+    projectTypes.Add(new GLProjectType
+    {
+        ProjectTypeCode = $"Code{i}",
+        ProjectTypeName = $"Name{i}"
+    });
+}
+
+
+//为 projectTypes 中每个实例生成 10-50个 GLProject 实例
+var projects = new List<GLProject>();
+for (int i = 0; i < projectTypes.Count; i++)
+{
+    var projectType = projectTypes[i];
+    var projectCount = new Random().Next(10, 50);
+    for (int j = 0; j < projectCount; j++)
+    {
+        projects.Add(new GLProject
+        {
+            ProjectTypeCode = projectType.ProjectTypeCode,
+            ProjectName = $"{projectType.ProjectTypeCode}-ProjectName{j}",
+            ProjectCode = $"{projectType.ProjectTypeCode}-ProjectCode{j}"
+        });
+    }
+}
+
+
+
+
+
+
+class GLProjectType
+{ 
+    public string ProjectTypeCode { get; set; }
+
+    public string ProjectTypeName { get; set; }
+}
+
+class GLProject
+{
+    public string ProjectTypeCode { get; set; }
+
+    public string ProjectName { get; set; }
+    public string ProjectCode { get; set; }
+}
